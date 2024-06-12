674c65cfa03ab74a5bb087755953a4112a601f8c
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-12 10:43:49 +0200

    Updated index.ts: reformatted code, added semicolons, and added new 'properties' array with an object.

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { showReviewTotal, populateUser } from './utils'
-let isOpen: boolean
+import { showReviewTotal, populateUser } from "./utils";
+let isOpen: boolean;
 
 //Reviews
-const reviews : {
-    name: string;
-    stars: number;
-    loyaltyUser: boolean;
-    date: string;
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
 }[] = [
   {
     name: "Sheia",
@@ -30,21 +30,49 @@ const reviews : {
 
 //User
 const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[];
-}
-= {
-    firstName: "Bobby",
-    lastName: 'Brown',
-    isReturning: true,
-    age: 35,
-    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+  firstName: string;
+  lastName: string;
+  isReturning: boolean;
+  age: number;
+  stayedAt: string[];
+} = {
+  firstName: "Bobby",
+  lastName: "Brown",
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
+
+//Properties
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstline: string;
+    city: string;
+    code: number;
+    country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [
+  {
+    image: 'holder.png',
+    title: 'Rustenburg Lodge',
+    price: 500,
+    location: {
+      firstline: 'Building 48',
+      city: 'Rustenburg',
+      code: 322,
+      country: 'South Africa',
+    },
+    contact: '014-518-0144',
+    isAvailable: true
+  }
+];
 
 //Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
-populateUser(you.isReturning, you.firstName)
+populateUser(you.isReturning, you.firstName);
