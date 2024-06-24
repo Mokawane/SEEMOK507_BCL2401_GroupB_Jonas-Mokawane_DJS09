6bfd93ac6122f60df8b6bf803354a1c6a9f1e09e
Jonas <jonas.mokawane98@gmail.com>
2024-06-24 20:58:50 +0200

    updated properties

--- typesAlias.ts
+++ typesAlias.ts
@@ -1,3 +1,3 @@
 //Type Alias
-export type Price = 500 | 4800 | 900
-export type Country = 'South Africa' | 'South Africa' | 'South Africa' 
+export type Price = 500 | 4800| 35 | 900
+export type Country = 'South Africa' | 'South Africa' | 'South Africa' | 'Malaysia'
